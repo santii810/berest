1982252fe345cb80a98451e0706cb67fe0eb6825
sgvilar <sgvilar@esei.uvigo.es>
2019-02-20 19:30:45 +0100

    Generated proyect and inyect dependencies

---
+++ src/main/java/es/uvigo/esei/sgvilar/berest/BerestApplication.java
@@ -0,0 +1,13 @@
+package es.uvigo.esei.sgvilar.berest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerestApplication.class, args);
+	}
+
+}
