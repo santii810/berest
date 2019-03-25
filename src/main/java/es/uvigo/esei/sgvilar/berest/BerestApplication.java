cd1267c47c6e6c5085589e7ba5337e708bebbf9b
Santiago Gómez Vilar <sgvilar@esei.uvigo.es>
2019-03-25 01:24:02 +0100

    Starts to implement autentication methods

--- src/main/java/es/uvigo/esei/sgvilar/berest/BerestApplication.java
+++ src/main/java/es/uvigo/esei/sgvilar/berest/BerestApplication.java
@@ -2,12 +2,29 @@ package es.uvigo.esei.sgvilar.berest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class BerestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BerestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BerestApplication.class, args);
+    }
 
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/user").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
 }
--- src/main/java/es/uvigo/esei/sgvilar/berest/controllers/UserController.java
+++ src/main/java/es/uvigo/esei/sgvilar/berest/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
@@ -27,4 +28,14 @@ public class UserController {
     public Iterable<UserEntity> findAll() {
         return userService.findAll();
     }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public String login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+        return "Autenticado";
+    }
+
+
 }
