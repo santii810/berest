8b19e684b7c417abcfd7240dd3c06df0cf2e9337
Santiago Gómez Vilar <santii810@hotmail.com>
2019-06-03 17:34:46 +0200

    Adds save user method

--- src/main/java/es/uvigo/esei/sgvilar/berest/controllers/UserController.java
+++ src/main/java/es/uvigo/esei/sgvilar/berest/controllers/UserController.java
@@ -1,16 +1,15 @@
 package es.uvigo.esei.sgvilar.berest.controllers;
 
 import es.uvigo.esei.sgvilar.berest.entities.UserEntity;
-import es.uvigo.esei.sgvilar.berest.services.GroupService;
 import es.uvigo.esei.sgvilar.berest.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
@@ -21,8 +20,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @Autowired
-    private GroupService groupService;
 
 
     @RequestMapping(
@@ -36,24 +33,14 @@ public class UserController {
 
     @RequestMapping(
             method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<String> save(
-            @RequestParam("groupId") int groupId,
-            @RequestParam("user") String name,
-            @RequestParam("organization") String organization,
-            @RequestParam("email") String email,
-            @RequestParam("password") String pwd
+    public ResponseEntity<String> addUser(
+            @RequestBody UserEntity user
     ) {
-        UserEntity user = new UserEntity();
-        user.setName(name);
-        user.setPassword(pwd);
-        user.setEmail(email);
-        user.setOrganization(organization);
-        user.setGroup(groupService.findById(groupId));
         UserEntity savedUser = userService.save(user);
         return new ResponseEntity<>("User created " + savedUser.getId(), HttpStatus.CREATED);
-
     }
 
 
